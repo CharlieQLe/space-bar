ef5987df3f73d4abb52e8ff7e53e64f3ebe73df1
Christopher Lübbemeier <luebbemeier@metaventis.com>
2022-04-07 22:14:41 +0200

    traspile "import { ... }"

--- scripts/build.sh
+++ scripts/build.sh
@@ -23,7 +23,11 @@ function fixupJavaScript() (
 			-e 's#Object.defineProperty(exports, "__esModule", { value: true });#var exports = {};#g' \
 			"${file}"
 		sed -i -E 's/export class (\w+)/var \1 = class \1/g' "${file}"
-		sed -i -E "s/import \* as (\w+) from '(\w+)'/const \1 = Me.imports.\2/g" "${file}"
+		sed -i -E "s/import \* as (\w+) from '(.+)'/const \1 = Me.imports.\2/g" "${file}"
+		# Replace import statements of the style "import { Foo } from 'foo';"
+		sed -i -E "s/^import \{(.*)\} from '(.*)';$/const {\1} = Me.imports.\2;/g" "${file}"
+		# Replace slashes with dots in lines containing "Me.imports."
+		sed -i -E "/^const .* = Me\.imports\..*;/ s/(\w)\/(\w)/\1.\2/g" "${file}"
 	done
 )
 
